6b3352c45ed84b54ef6438a8bfed83b24b14f031
rahul shitole <shitolerahul1@hotmail.com>
2021-05-15 11:27:15 +0530

    Step two client app

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,10 +33,12 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +54,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
